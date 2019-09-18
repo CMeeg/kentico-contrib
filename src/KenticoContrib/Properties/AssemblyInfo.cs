78cb3eb4309458025e34a86cced885db9072abd3
Chris Meagher <cpmeeg@gmail.com>
2019-09-18 23:11:41 +0100

    Install CMS

---
+++ src/KenticoContrib/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("KenticoContrib")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("3924d5c0-6a63-40f0-b5a0-61e516233f69")]
+
+[assembly: AssemblyProduct("KenticoContrib")]
+[assembly: AssemblyCopyright("© 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyVersion("12.0.0.0")]
+[assembly: AssemblyFileVersion("12.0.7195.15164")]
+[assembly: AssemblyInformationalVersion("12.0.39")]
