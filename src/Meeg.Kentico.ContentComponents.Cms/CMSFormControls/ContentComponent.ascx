40b9453f35cf3b84b5792dc89f52a98ebcbb1f75
Chris Meagher <cpmeeg@gmail.com>
2019-09-20 00:09:39 +0100

    Add content component form control

---
+++ src/Meeg.Kentico.ContentComponents.Cms/CMSFormControls/ContentComponent.ascx
@@ -0,0 +1,9 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="ContentComponent.ascx.cs" Inherits="Meeg.Kentico.ContentComponents.Cms.CMSFormControls.ContentComponent" %>
+
+<cms:BasicForm ID="ContentComponentForm" runat="server"
+    ShortID="cc"
+    HtmlAreaToolbarLocation="Out:CKToolbar"
+    IsLiveSite="false"
+    MarkRequiredFields="true"
+    ShowValidationErrorMessage="false"
+/>
---
+++ src/Meeg.Kentico.ContentComponents.Cms/CMSFormControls/ContentComponent.ascx.cs
@@ -0,0 +1,251 @@
+﻿using CMS.DataEngine.Serialization;
+using CMS.DocumentEngine;
+using CMS.FormEngine;
+using CMS.FormEngine.Web.UI;
+using CMS.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Xml;
+
+namespace Meeg.Kentico.ContentComponents.Cms.CMSFormControls
+{
+    public partial class ContentComponent : FormEngineUserControl
+    {
+        private string PageType
+        {
+            get
+            {
+                return GetValue("PageType", string.Empty);
+            }
+            set
+            {
+                SetValue("PageType", value);
+            }
+        }
+
+        private string AlternativeFormName
+        {
+            get
+            {
+                return GetValue("AlternativeFormName", string.Empty);
+            }
+            set
+            {
+                SetValue("AlternativeFormName", value);
+            }
+        }
+
+        private string PageTypeFormName
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(AlternativeFormName))
+                {
+                    return PageType;
+                }
+
+                return $"{PageType}.{AlternativeFormName}";
+            }
+        }
+
+        public override object Value
+        {
+            get
+            {
+                // Get the component fields and use the field data to create a TreeNode representing the component
+
+                ContentComponentFieldCollection componentFields = GetContentComponentFormFields();
+                ContentComponentNode = GetContentComponentNode(componentFields);
+
+                // Use the field data to set any system fields on the page this component belongs to
+
+                SetPageSystemFields(componentFields);
+
+                // Return the component node serialised to XML
+
+                XmlElement xml = ContentComponentNode.Serialize();
+                return xml.OuterXml;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    return;
+                }
+
+                string componentXml = value.ToString();
+
+                if (string.IsNullOrEmpty(componentXml))
+                {
+                    return;
+                }
+
+                // Deserialise the component XML to a TreeNode that represents the component
+
+                ContentComponentNode = GetContentComponentFromXml(componentXml);
+            }
+        }
+
+        private TreeNode ContentComponentNode
+        {
+            get;
+            set;
+        }
+
+        protected override void OnInit(EventArgs e)
+        {
+            base.OnInit(e);
+
+            LoadContentComponentForm();
+        }
+
+        private void LoadContentComponentForm()
+        {
+            // Set some basic form properties
+
+            ContentComponentForm.SubmitButton.Visible = false;
+            ContentComponentForm.MessagesPlaceHolder = MessagesPlaceHolder;
+
+            // TODO: Resource string  
+            ValidationError = "Please correct the above validation errors.";
+
+            // Load and populate the form
+
+            FormInfo form = FormHelper.GetFormInfo(PageTypeFormName, true);
+
+            ContentComponentForm.FormInformation = form;
+
+            if (ContentComponentNode != null)
+            {
+                // Populate form with existing data
+
+                ContentComponentForm.Data = ContentComponentNode;
+                ContentComponentForm.EditedObject = ContentComponentNode;
+            }
+            else
+            {
+                // Populate form with default data
+
+                DataRow dr = form.GetDataRow();
+
+                form.LoadDefaultValues(dr, true);
+
+                ContentComponentForm.DataRow = dr;
+            }
+
+            // Reload the form to populate the UI controls with component data
+
+            ContentComponentForm.ReloadData();
+        }
+
+        private ContentComponentFieldCollection GetContentComponentFormFields()
+        {
+            var componentFieldCollection = new ContentComponentFieldCollection();
+
+            ContentComponentForm.Fields.ForEach(fieldName =>
+            {
+                FormEngineUserControl field = ContentComponentForm.FieldControls[fieldName];
+
+                object fieldValue = field.Value;
+                bool fieldIsSystem = field.FieldInfo.System;
+
+                componentFieldCollection.AddField(fieldName, fieldValue, fieldIsSystem);
+            });
+
+            return componentFieldCollection;
+        }
+
+        private TreeNode GetContentComponentNode(ContentComponentFieldCollection componentFields)
+        {
+            var componentNode = TreeNode.New(PageType);
+
+            componentFields.ApplyFieldsTo(componentNode);
+
+            return componentNode;
+        }
+
+        private void SetPageSystemFields(ContentComponentFieldCollection componentFields)
+        {
+            var page = Form.EditedObject as TreeNode;
+
+            componentFields.ApplyFieldsTo(page, field => field.IsSystem);
+        }
+
+        private TreeNode GetContentComponentFromXml(string xml)
+        {
+            DataSet contentData = DataHelper.GetDataSetFromXml(xml);
+
+            if (DataHelper.DataSourceIsEmpty(contentData))
+            {
+                return null;
+            }
+
+            return TreeNode.New(PageType, contentData.Tables[0].Rows[0]);
+        }
+
+        public override bool IsValid()
+        {
+            return ContentComponentForm.ValidateData();
+        }
+
+        private class ContentComponentFieldCollection
+        {
+            private List<Field> Fields { get; }
+
+            public ContentComponentFieldCollection()
+            {
+                Fields = new List<Field>();
+            }
+
+            public void AddField(string name, object value, bool isSystem)
+            {
+                Fields.Add(new Field(name, value, isSystem));
+            }
+
+            public void ApplyFieldsTo(TreeNode node, Func<Field, bool> fieldPredicate = null)
+            {
+                if (node == null)
+                {
+                    return;
+                }
+
+                foreach (Field field in Fields.Where(f => fieldPredicate == null ? true : fieldPredicate.Invoke(f)))
+                {
+                    // Prefer setting TreeNode properties over using TreeNode.SetValue in case there is any specific logic in the property setter that wouldn't be executed when using TreeNode.SetValue
+
+                    PropertyInfo fieldProperty = node.GetType().GetProperty(field.Name, BindingFlags.Public | BindingFlags.Instance);
+
+                    if (fieldProperty != null && fieldProperty.CanWrite)
+                    {
+                        // There is a corresponding property we can set
+
+                        fieldProperty.SetValue(node, field.Value);
+                    }
+                    else
+                    {
+                        // No property, so use SetValue instead
+
+                        node.SetValue(field.Name, field.Value);
+                    }
+                }
+            }
+
+            public struct Field
+            {
+                public string Name { get; }
+                public object Value { get; }
+                public bool IsSystem { get; }
+
+                public Field(string name, object value, bool isSystem)
+                {
+                    Name = name;
+                    Value = value;
+                    IsSystem = isSystem;
+                }
+            }
+        }
+    }
+}
