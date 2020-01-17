0aea9d9b5f2361b23c0b481ada49613e3f6043e6
Chris Meagher <cpmeeg@gmail.com>
2020-01-17 22:53:58 +0000

    Get and use favicon path from CMS config settings
    This is just a simple example of getting and using CMS settings using the Config Builder.

--- src/KenticoContrib/Features/Layout/LayoutController.cs
+++ src/KenticoContrib/Features/Layout/LayoutController.cs
@@ -1,16 +1,21 @@
-﻿using System.Threading.Tasks;
+using System.Text;
+using System.Threading.Tasks;
 using System.Web.Mvc;
 using MediatR;
+using Meeg.Configuration;
+using Meeg.Kentico.Configuration.Cms;
 
 namespace KenticoContrib.Features.Layout
 {
     public class LayoutController : Controller
     {
         private readonly IMediator mediator;
+        private readonly IAppConfigurationRoot appConfig;
 
-        public LayoutController(IMediator mediator)
+        public LayoutController(IMediator mediator, IAppConfigurationRoot appConfig)
         {
             this.mediator = mediator;
+            this.appConfig = appConfig;
         }
 
         [ChildActionOnly]
@@ -24,6 +29,38 @@ namespace KenticoContrib.Features.Layout
             }
 
             return PartialView("_PageMetadata", viewModel);
+        }
+
+        [ChildActionOnly]
+        public ActionResult Favicon()
+        {
+            // TODO: Get current site name rather than hardcode
+
+            string currentSiteName = "KenticoContrib";
+
+            var faviconOptions = appConfig
+                .Get<FaviconOptions>("KenticoContrib");
+
+            string faviconPath = faviconOptions.CmsFaviconPath;
+
+            if (string.IsNullOrEmpty(faviconPath))
+            {
+                return new EmptyResult();
+            }
+
+            string faviconHtml = $"<link rel=\"shortcut icon\" href=\"{faviconPath}\">";
+
+            return new ContentResult
+            {
+                Content = faviconHtml,
+                ContentType = "text/html",
+                ContentEncoding = Encoding.UTF8
+            };
+        }
+
+        private class FaviconOptions
+        {
+            public string CmsFaviconPath { get; set; }
         }
     }
 }
